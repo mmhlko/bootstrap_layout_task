b97f0ef5cc712130c434c8a346cda1eb2977150a
mmhlko <maximmhl@mail.ru>
2023-06-28 01:53:37 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const singupBtn = document.querySelector('.singup__btn')
+const sendButton = document.querySelector('#singup__send_btn')
+const singUpModal = document.querySelector('.modal__form')
+
+
+singupBtn.onclick = (e) => {
+    e.preventDefault();
+    document.querySelector('.singup__modal_body').classList.remove('hidden')
+    document.querySelector('.form__send').classList.add('hidden')
+}  
+
+sendButton.onclick = (e) => {
+    e.preventDefault();
+    document.querySelector('.singup__modal_body').classList.add('hidden')
+    document.querySelector('.form__send').classList.remove('hidden')
+}  
+
+
+let code = document.querySelector('#code'); // Получаем скрытый input
+  document.querySelector('.btn').onclick = function(){ // Клик по кнопке отправки
+    code.value = 'NOSPAM'; // Подставляем значение в value инпута
+  };
