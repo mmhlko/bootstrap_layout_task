480c7ca995943008af3380a3086db3ad122e6ce0
mmhlko <maximmhl@mail.ru>
2023-06-29 01:12:34 +0700

    form js edit

--- js/index.js
+++ js/index.js
@@ -1,27 +1,26 @@
 const singupBtn = document.querySelector('#singup__btn')
-const sendButton = document.querySelector('#send__form_modal')
-/* const singUpModal = document.querySelector('#modal__form') */
 
+const formModal = document.getElementById('form__modal')
 
-singupBtn.onclick = (e) => {
-    e.preventDefault();
-    document.querySelector('.singup__modal_body').classList.remove('hidden')
-    document.querySelector('.form__send').classList.add('hidden')
-}  
+singupBtn.addEventListener('click', () => {
+  document.querySelector('.singup__modal_body').classList.remove('hidden')
+  document.querySelector('.form__send').classList.add('hidden')
+  document.querySelectorAll('input, textarea').forEach(item => item.value = '')
+})
 
-sendButton.onclick = (e) => {
-    e.preventDefault();
-    document.querySelector('.singup__modal_body').classList.add('hidden')
-    document.querySelector('.form__send').classList.remove('hidden')
-}  
+formModal.addEventListener('submit', function (event) {
+  event.preventDefault();
+  document.querySelector('.singup__modal_body').classList.add('hidden')
+  document.querySelector('.form__send').classList.remove('hidden')
+})
 
 
 let code = document.querySelector('#code'); // Получаем скрытый input
-  document.querySelector('.btn').onclick = function(){ // Клик по кнопке отправки
-    code.value = 'NOSPAM'; // Подставляем значение в value инпута
-  };
+document.querySelector('.btn').onclick = function () { // Клик по кнопке отправки
+  code.value = 'NOSPAM'; // Подставляем значение в value инпута
+};
 
-  let code_modal = document.querySelector('#code_modal'); // Получаем скрытый input
-  document.querySelector('.btn').onclick = function(){ // Клик по кнопке отправки
-    code.value = 'NOSPAM'; // Подставляем значение в value инпута
-  };
+let code_modal = document.querySelector('#code_modal'); // Получаем скрытый input
+document.querySelector('.btn').onclick = function () { // Клик по кнопке отправки
+  code.value = 'NOSPAM'; // Подставляем значение в value инпута
+};
